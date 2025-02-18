45260da1359b30f7f25eb2869cf8e0305ce4f65c
park8306 <kosaf0804@gmail.com>
2025-02-18 17:55:39 +0900

    [ feat ] Camera 영역 제한 기능 추가

--- Assets/Scripts/Camera/FollowCamera.cs
+++ Assets/Scripts/Camera/FollowCamera.cs
@@ -7,7 +7,10 @@ public class FollowCamera : MonoBehaviour
     public Transform target;
     Vector3 offset;     // 타겟과 카메라의 거리
     [SerializeField] private float cameraSpeed = 1f;    // 카메라 움직이는 속도 조절
-    //[SerializeField] Vector2 mapSize;
+    [SerializeField] Vector2 mapSize;
+
+    float width;
+    float height;
 
     private void Start()
     {
@@ -16,6 +19,12 @@ public class FollowCamera : MonoBehaviour
 
         offset = transform.position - target.position; // 타겟에서 카메라의 거리를 계산
         Debug.Log(new Vector2(Screen.width, Screen.height));
+
+        // orthographicSize를 바탕으로 카메라 범위 구하기
+        height = Camera.main.orthographicSize;
+        width = height * ((float)Screen.width / Screen.height);
+
+        Debug.Log("height:" + height + "widht :" + width);
     }
 
     private void Update()
@@ -28,12 +37,20 @@ public class FollowCamera : MonoBehaviour
         pos = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime * cameraSpeed);
         transform.position = pos;
 
+        // 카메라 영역 제한
 
+        float limitX = mapSize.x - width; // 카메라가 최대한 갈 수 있는 가로 거리
+        float clampX = Mathf.Clamp(transform.position.x, -limitX, limitX);
+
+        float limitY = mapSize.y - height; // 카메라가 최대한 갈 수 있는 세로 거리
+        float clampY = Mathf.Clamp(transform.position.y, -limitY, limitY);
+
+        transform.position = new Vector3(clampX, clampY, transform.position.z);
     }
 
-    //private void OnDrawGizmos()
-    //{
-    //    Gizmos.color = Color.red;
-    //    Gizmos.DrawWireCube(Vector3.zero, mapSize * 2);
-    //}
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(Vector3.zero, mapSize * 2);
+    }
 }
