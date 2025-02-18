1a939d12d076ef16c5120233b253025954cb9ed6
park8306 <kosaf0804@gmail.com>
2025-02-18 17:38:19 +0900

    [ feat ] FollowCamera 따라다니는 카메라 구현

--- Assets/Scripts/Camera/FollowCamera.cs
+++ Assets/Scripts/Camera/FollowCamera.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform target;
+    Vector3 offset;     // 타겟과 카메라의 거리
+    [SerializeField] private float cameraSpeed = 1f;    // 카메라 움직이는 속도 조절
+    //[SerializeField] Vector2 mapSize;
+
+    private void Start()
     {
-        
+        if (target == null)
+            return;
+
+        offset = transform.position - target.position; // 타겟에서 카메라의 거리를 계산
+        Debug.Log(new Vector2(Screen.width, Screen.height));
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (target == null)
+            return;
+
+        Vector3 pos = transform.position;
+        // 부드러운 움직임을 위해 lerp
+        pos = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime * cameraSpeed);
+        transform.position = pos;
+
+
     }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawWireCube(Vector3.zero, mapSize * 2);
+    //}
 }
