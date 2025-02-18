af6cf9cfd7cab5005f77908ee13594244088b1e0
park8306 <kosaf0804@gmail.com>
2025-02-18 20:00:34 +0900

    [ design ] FlappyPlane용 시작 UI 생성

---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Minigames
+{
+    Flappy,
+    Stack,
+    Count
+}
+
+public class UIManager : MonoBehaviour
+{
+    private Minigames miniGames;
+}
